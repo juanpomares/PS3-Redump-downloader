c347b31bd62a55a3ae5a6f0488e4a20e24fe5a98
Juan Pomares Bernabeu <juan_93_pomares@hotmail.com>
2024-03-31 09:57:31 +0200

    🎉 Create project

---
+++ main.py
@@ -0,0 +1,5 @@
+
+PS3_ISO_LINK='https://myrient.erista.me/files/Redump/Sony%20-%20PlayStation%203/'
+PS3_KEYS_LINK='https://myrient.erista.me/files/Redump/Sony%20-%20PlayStation%203%20-%20Disc%20Keys/'
+
+
