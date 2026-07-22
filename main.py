ebae3f67ec94e1cc759838a14fdb6af86ae8a3b7
Juan Pomares Bernabeu <juan_93_pomares@hotmail.com>
2026-07-22 22:19:54 +0200

    ✨ Add torrent file download functionality

--- main.py
+++ main.py
@@ -14,6 +14,9 @@ from bs4 import BeautifulSoup
 from tqdm import tqdm
 from tqdm.utils import CallbackIOWrapper
 
+import re
+from playwright.sync_api import expect, sync_playwright
+
 config = {}
 TMP_FOLDER_PATHNAME = ''
 TMP_ISO_FOLDER_PATHNAME = ''
@@ -49,8 +52,7 @@ def getPS3List():
             link_element = current_link.select('a')[0]
             size_element = current_link.select('span')[0]
 
-            available_ps3_titles.append(
-                {'title': link_element.text, 'link': link_element['href'], 'size': size_element.text})
+            available_ps3_titles.append({'title': link_element.text, 'size': size_element.text})
 
         except:
             pass
@@ -207,12 +209,58 @@ def removeFiles(files):
         removeFile(file)
 
 
-def downloadAndUnzip(route, title, isISO):
+def getTorrentFile(baseRoute, file, file_path):
+    link = baseRoute.replace('browse', 'rom/?name=.') + file + '.zip'
+    torrent_file_path = os.path.join(file_path, "temp.torrent")
+    if os.path.exists(torrent_file_path):
+        print(f"Torrent file already exists at {torrent_file_path}. Skipping download.")
+        return torrent_file_path
+
+    print(f"Downloading torrent file...")
+
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(headless=True)
+
+        try:
+            context = browser.new_context(accept_downloads=True)
+
+            page = context.new_page()
+            page.goto(link, wait_until="domcontentloaded", timeout=60_000)
+
+            download_link = page.locator("#download")
+
+            expect(download_link).to_have_attribute(
+                "href",
+                re.compile(r"\.torrent(?:\?.*)?$"),
+                timeout=60_000,
+            )
+
+            with page.expect_download(timeout=60_000) as download_info:
+                download_link.click()
+
+            download = download_info.value
+            download.save_as(torrent_file_path)
+
+        finally:
+            browser.close()
+
+    print(f"Torrent file downloaded: {torrent_file_path}")
+
+    return torrent_file_path
+
+
+
+def downloadAndUnzip(route, title, isISO):    
     is_iso_string = "ISO" if isISO else "Key"
     print(f" # {is_iso_string} file...")
 
+    tmp_path = TMP_ISO_FOLDER_PATHNAME if isISO else TMP_KEY_FOLDER_PATHNAME
+    torrent_file_path = getTorrentFile(route, title, tmp_path)
+
+    ## TODO Donwload file using torrent_file_path
+    
     unzipped_file_name = f"{title}.{'iso' if isISO else 'dkey'}"
-    unzipped_file_path = os.path.join(TMP_ISO_FOLDER_PATHNAME if isISO else TMP_KEY_FOLDER_PATHNAME, unzipped_file_name)
+    unzipped_file_path = os.path.join(tmp_path, unzipped_file_name)
 
     if os.path.exists(unzipped_file_path):
         print(' - File previously downloaded :)', end='\n\n')
@@ -275,13 +323,12 @@ def decryptFile(gameName):
 
 
 def downloadPS3Element(element):
-    link = element['link']
     title = element['title'].replace(".zip", "")
 
     print(f"\nSelected {title}\n")
 
-    downloadAndUnzip(config['ISO_URL'] + link, title, True)
-    downloadAndUnzip(config['KEY_URL'] + link, title, False)
+    downloadAndUnzip(config['ISO_URL'], title, isISO=True)
+    downloadAndUnzip(config['KEY_URL'], title, isISO=False)
     print(f'\n{title} downloaded :)')
     decryptFile(title)
 
