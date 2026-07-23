76554f358b142d02a71a100bbb6b95fe5ebbe3e2
Juan Pomares Bernabeu <juan_93_pomares@hotmail.com>
2026-07-23 19:24:02 +0200

    ✨ Add openPlaywrightBrowser function to launch compatible browsers

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from tqdm import tqdm
 from tqdm.utils import CallbackIOWrapper
 
 import re
-from playwright.sync_api import expect, sync_playwright
+from playwright.sync_api import Error as PlaywrightError, expect, sync_playwright
 
 config = {}
 TMP_FOLDER_PATHNAME = ''
@@ -209,6 +209,29 @@ def removeFiles(files):
         removeFile(file)
 
 
+def openPlaywrightBrowser(playwright):
+    browsers = [("chrome", "Google Chrome"), ("msedge", "Microsoft Edge"),]
+
+    for channel, browser_name in browsers:
+        try:
+            browser = playwright.chromium.launch(
+                channel=channel,
+                headless=False,
+            )
+
+            print(f"Using {browser_name} with Playwright.")
+            return browser
+
+        except PlaywrightError:
+            print(f"{browser_name} not found. Trying next browser...")
+            pass
+
+    raise RuntimeError(
+        "No compatible browser was found.\n"
+        "Please install Microsoft Edge or Google Chrome."
+    )
+
+
 def getTorrentFile(baseRoute, file, file_path):
     link = baseRoute.replace('browse', 'rom/?name=.') + file + '.zip'
     torrent_file_path = os.path.join(file_path, "temp.torrent")
@@ -219,7 +242,7 @@ def getTorrentFile(baseRoute, file, file_path):
     print(f"Downloading torrent file...")
 
     with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=True)
+        browser = openPlaywrightBrowser(playwright)
 
         try:
             context = browser.new_context(accept_downloads=True)
